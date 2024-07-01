38fd1f72a3349d90b16904bf4a97dd43bbae342f
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-01 19:07:22 +0200

    Add project files.

---
+++ DepthFirstSearch/Nyja.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepthFirstSearch
+{
+    internal class Nyja
+    {
+        public int Vlera { get; set; }
+        public Nyja DegaMajte { get; set; } //e tipit Nyja sepse referon te nje nyje tjeter
+        public Nyja DegaDjathte { get; set; }
+    }
+
+    internal class PemaBinare
+    {
+        public Nyja Rrenja { get; set; }
+
+        public bool ShtoNyje(int vlere)
+        {
+            Nyja pas = Rrenja;
+            Nyja para = null;
+
+            while (pas != null) //kur te mberrijme ne fund te pemes
+            {
+                para = pas;
+                if (vlere < pas.Vlera)
+                {
+                    pas = pas.DegaMajte;
+                }
+                else if (vlere > pas.Vlera)
+                {
+                    pas = pas.DegaDjathte;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Nyja NyjaRe = new Nyja();
+            NyjaRe.Vlera = vlere;
+
+            if (Rrenja == null)
+            {
+                Rrenja = NyjaRe;
+            }
+            else
+            {
+
+                if (vlere < para.Vlera)
+                {
+                   para.DegaMajte=NyjaRe;
+                }
+                else
+                {
+                   para.DegaDjathte=NyjaRe;
+                }
+            }
+            return true;
+        }
+
+        //depth first search
+        public void DFS(Nyja rrenja, int shuma)
+        {
+            if (rrenja != null)
+            {
+                shuma += rrenja.Vlera;
+                DFS(rrenja.DegaMajte, shuma);
+                DFS(rrenja.DegaDjathte, shuma);
+
+                if ((rrenja.DegaMajte==null) && (rrenja.DegaDjathte==null))
+                {
+                    Console.WriteLine(shuma);
+                }
+            }
+        }
+    }
+}
---
+++ DepthFirstSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using DepthFirstSearch;
+
+PemaBinare pemaBinare=new PemaBinare();
+
+pemaBinare.ShtoNyje(3);
+pemaBinare.ShtoNyje(1);
+pemaBinare.ShtoNyje(2);
+pemaBinare.ShtoNyje(7);
+pemaBinare.ShtoNyje(6);
+pemaBinare.ShtoNyje(8);
+
+pemaBinare.DFS(pemaBinare.Rrenja, 0);
